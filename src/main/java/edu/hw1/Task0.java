6924a6f599f5f1fa37dcf604a27abddcb689be91
aleckbb <belozerov200@gmail.com>
2023-10-08 19:15:35 +0300

    Task0(1)

--- src/main/java/edu/hw1/Task0.java
+++ src/main/java/edu/hw1/Task0.java
@@ -1,6 +1,5 @@
 package edu.hw1;
 
-import java.util.Objects;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,7 +7,6 @@ public class Task0 {
     private final static Logger LOGGER = LogManager.getLogger();
 
     public static void main(String[] args) {
-
         LOGGER.info("Привет, мир!");
     }
 }
