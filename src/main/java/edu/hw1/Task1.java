aef59feed001cfaf16b1a84b51d389051350abb4
aleckbb <belozerov200@gmail.com>
2023-10-08 23:50:34 +0300

    Task0(4)

--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -1,5 +1,6 @@
 package edu.hw1;
 
+import java.util.Scanner;
 import java.util.Objects;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,7 +12,47 @@ public final class Task1 {
     private Task1() {
     }
 
+    public static boolean check(String str) {
+        Objects.requireNonNull(str);
+        boolean res = true;
+        if (!str.contains(":") || str.indexOf(':') == 0 || str.indexOf(':') == str.length() - 1 ||
+            !(str.indexOf(':') == str.lastIndexOf(':'))) {
+            res = false;
+        } else {
+            for (int i = 0; i < str.length() && res; i++) {
+                if (!(str.charAt(i) >= '0' && str.charAt(i) <= '9') && str.charAt(i) != ':') {
+                    res = false;
+                }
+            }
+            if (res) {
+                String[] sarr = str.split(":");
+                if (Integer.parseInt(sarr[0]) < 0 || Integer.parseInt(sarr[1]) < 0 || Integer.parseInt(sarr[1]) >= 60) {
+                    res = false;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static int minutesToSeconds(String str) {
+
+        int res;
+        if (!check(str)) {
+            res = -1;
+        } else {
+            String[] sarr = str.split(":");
+            res = Integer.parseInt(sarr[0]) * 60 + Integer.parseInt(sarr[1]);
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LOGGER.info("Введите время(mm:ss).");
+        String str = scanner.nextLine();
+        LOGGER.info("Длина видео -> " + minutesToSeconds(str));
+        scanner.close();
 
     }
 }
--- src/test/java/edu/hw1/Task1Test.java
+++ src/test/java/edu/hw1/Task1Test.java
@@ -1,13 +1,42 @@
 package edu.hw1;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class Task1Test {
+public class Task1Test {
 
     @Test
-    void main() {
-
+    @DisplayName("Конвертация времени в секунды")
+    void minuteToSeconds() {
+        int actual = Task1.minutesToSeconds("1::1");
+        int expected = -1;
+        assertEquals(expected, actual);
+        actual = Task1.minutesToSeconds("-1:1");
+        expected = -1;
+        assertEquals(expected, actual);
+        actual = Task1.minutesToSeconds("1:-1");
+        expected = -1;
+        assertEquals(expected, actual);
+        actual = Task1.minutesToSeconds(":11");
+        expected = -1;
+        assertEquals(expected, actual);
+        actual = Task1.minutesToSeconds("11:");
+        expected = -1;
+        assertEquals(expected, actual);
+        actual = Task1.minutesToSeconds("aa:1");
+        expected = -1;
+        assertEquals(expected, actual);
+        actual = Task1.minutesToSeconds("1:aa");
+        expected = -1;
+        assertEquals(expected, actual);
+        actual = Task1.minutesToSeconds("1:60");
+        expected = -1;
+        assertEquals(expected, actual);
+        actual = Task1.minutesToSeconds("0:0");
+        expected = 0;
+        assertEquals(expected, actual);
     }
 }
