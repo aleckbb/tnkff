2174b80756e8e27a9fbe070039064ce1ef5ebc27
aleckbb <belozerov200@gmail.com>
2023-10-09 20:35:19 +0300

    Task0(4)

--- src/main/java/edu/hw1/Task8.java
+++ src/main/java/edu/hw1/Task8.java
@@ -15,19 +15,32 @@ public class Task8 {
     private Task8() {
     }
 
+    public static boolean top(int[][] a, int i, int j){
+        return ( i + TWO < EIGHT && j + ONE < EIGHT && a[i + TWO][j + ONE] == ONE
+            || i + TWO < EIGHT && j - ONE >= 0 && a[i + TWO][j - ONE] == ONE);
+    }
+
+    public static boolean bottom(int[][] a, int i, int j){
+        return (i - TWO >= 0 && j + ONE < EIGHT && a[i - TWO][j + ONE] == ONE
+            || i - TWO >= 0 && j - ONE >= 0 && a[i - TWO][j - ONE] == ONE);
+    }
+
+    public static boolean left(int[][] a, int i, int j){
+        return (i - ONE >= 0 && j - TWO >= 0 && a[i - ONE][j - TWO] == ONE
+            || i + ONE < EIGHT && j - TWO >= 0 && a[i + ONE][j - TWO] == ONE);
+    }
+
+    public static boolean right(int[][] a, int i, int j){
+        return (i - ONE >= 0 && j + TWO < EIGHT && a[i - ONE][j + TWO] == ONE
+            ||i + ONE < EIGHT && j + TWO < EIGHT && a[i + ONE][j + TWO] == ONE);
+    }
+
     public static boolean knightBoardCapture(int[][] a) {
         boolean res = true;
         for (int i = 0; i < EIGHT && res; i++) {
             for (int j = 0; j < EIGHT && res; j++) {
                 if (a[i][j] == ONE) {
-                    if (i - TWO >= 0 && j + ONE < EIGHT && a[i - TWO][j + ONE] == ONE
-                        || i - TWO >= 0 && j - ONE >= 0 && a[i - TWO][j - ONE] == ONE
-                        || i - ONE >= 0 && j + TWO < EIGHT && a[i - ONE][j + TWO] == ONE
-                        || i - ONE >= 0 && j - TWO >= 0 && a[i - ONE][j - TWO] == ONE
-                        || i + ONE < EIGHT && j + TWO < EIGHT && a[i + ONE][j + TWO] == ONE
-                        || i + ONE < EIGHT && j - TWO >= 0 && a[i + ONE][j - TWO] == ONE
-                        || i + TWO < EIGHT && j + ONE < EIGHT && a[i + TWO][j + ONE] == ONE
-                        || i + TWO < EIGHT && j - ONE >= 0 && a[i + TWO][j - ONE] == ONE) {
+                    if (right(a, i, j) || left(a, i, j) || bottom(a, i, j) || top(a, i, j)) {
                         res = false;
                     }
                 }
