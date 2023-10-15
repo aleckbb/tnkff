90af55661a48d2e029ff64d8d787580e334fd628
aleckbb <belozerov200@gmail.com>
2023-10-15 23:03:18 +0300

    hw2 task1

---
+++ src/main/java/edu/hw2/Task4.java
@@ -0,0 +1,37 @@
+package edu.hw2;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Task4 {
+
+    private final static Logger LOGGER = LogManager.getLogger();
+
+    private Task4() {
+    }
+
+    public static void main(String[] args) {
+        LOGGER.info(callingInfo());
+    }
+
+    public record CallingInfo(String className, String methodName) {
+    }
+
+    public static CallingInfo callingInfo() {
+        String className, methodName;
+        try {
+            throw new Exception();
+        } catch (Throwable e) {
+
+            StackTraceElement[] stktrace = e.getStackTrace();
+            String str = stktrace[1].toString();
+            String str1 = str.substring(0, str.indexOf('('));
+            int splitIndex = str1.lastIndexOf('.');
+            className = str1.substring(0, splitIndex);
+            methodName = str1.substring(splitIndex + 1);
+
+        }
+        return new CallingInfo(className, methodName);
+    }
+
+}
