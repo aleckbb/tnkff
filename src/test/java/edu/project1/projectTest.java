4adf3492abd6075247c65e8be84c5609b05bbd6d
aleckbb <belozerov200@gmail.com>
2023-10-22 21:56:42 +0300

    project

--- src/test/java/edu/project1/projectTest.java
+++ src/test/java/edu/project1/projectTest.java
@@ -20,7 +20,8 @@ class projectTest {
         Game game = new Game("a\nb\nq\nd\ne\nE");
         game.start("fruct");
         String str = game.getOut();
-        String actual = str.substring(463, 472);
+        int index = str.indexOf("You lost!");
+        String actual = str.substring(index, index + 9);
         String expected = "You lost!";
         assertEquals(expected, actual);
     }
@@ -31,7 +32,8 @@ class projectTest {
         Game game = new Game("f\ngiveUp\nE");
         game.start("fruct");
         String str = game.getOut();
-        String actual = str.substring(114, 118);
+        int index = str.indexOf("Hit!");
+        String actual = str.substring(index, index + 4);
         String expected = "Hit!";
         assertEquals(expected, actual);
     }
@@ -42,8 +44,9 @@ class projectTest {
         Game game = new Game("y\ngiveUp\nE");
         game.start("fruct");
         String str = game.getOut();
-        String actual = str.substring(114, 141);
-        String expected = "Missed, mistake 1 out of 5.";
+        int index = str.indexOf("Missed, mistake 1 out of 5.");
+        String actual = str.substring(index, index + 26);
+        String expected = "Missed, mistake 1 out of 5";
         assertEquals(expected, actual);
     }
 
@@ -53,7 +56,8 @@ class projectTest {
         Game game = new Game("ty\ngiveUp\nE");
         game.start("rect");
         String str = game.getOut();
-        String actual = str.substring(113, 133);
+        int index = str.indexOf("Input correct letter");
+        String actual = str.substring(index, index + 20);
         String expected = "Input correct letter";
         assertEquals(expected, actual);
     }
