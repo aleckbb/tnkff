976b18b913503818ae1db276867fbc574f2de8a8
aleckbb <belozerov200@gmail.com>
2023-10-24 02:21:03 +0300

    hw3

---
+++ src/test/java/edu/hw3/Task1/Task3Test.java
@@ -0,0 +1,15 @@
+package edu.hw3.Task1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class Task3Test {
+
+    @Test
+    @DisplayName("Примеры из дз")
+    void test1() {
+        //ArrayList<Object> arrayList = new ArrayList<>(Arrays.asList("a", "bb", "a", "bb"));
+        //var actual = Task3.freqDict(Arrays.asList("a", "bb", "a", "bb"));
+        //assertEquals(new HashMap<Objects, Integer>(Map.<Objects, Integer>of("a", 2)), actual);
+    }
+}
