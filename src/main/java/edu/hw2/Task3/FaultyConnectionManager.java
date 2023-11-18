684fb1fd7ca6211c9a56da417e360c053dd863e7
aleckbb <belozerov200@gmail.com>
2023-11-19 01:14:42 +0300

    hw6 full

--- src/main/java/edu/hw2/Task3/FaultyConnectionManager.java
+++
@@ -1,7 +0,0 @@
-package edu.hw2.Task3;
-
-public class FaultyConnectionManager implements ConnectionManager {
-    public Connection getConnection() {
-        return new FaultyConnection();
-    }
-}
