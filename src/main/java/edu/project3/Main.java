b8d2a9546829a50d8d7c96278d43a05fbfa53f07
aleckbb <belozerov200@gmail.com>
2023-11-19 15:14:15 +0300

    project3

---
+++ src/main/java/edu/project3/Main.java
@@ -0,0 +1,14 @@
+package edu.project3;
+
+import java.time.OffsetDateTime;
+
+public class Main {
+
+    public static void main(String[] args) {
+        OffsetDateTime offsetDateTime = OffsetDateTime.parse("2007-12-03T10:15:30+03:00");
+        System.out.println(offsetDateTime);
+    }
+
+
+
+}
