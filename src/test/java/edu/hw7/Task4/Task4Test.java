14343f388334ebaa5efe7f1d86b8ae9a08c9d0c1
aleckbb <belozerov200@gmail.com>
2023-11-26 02:42:14 +0300

    hw7 full

--- src/test/java/edu/hw7/Task4/Task4Test.java
+++ src/test/java/edu/hw7/Task4/Task4Test.java
@@ -5,28 +5,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class Task4Test {
-
-    @Test
-    @DisplayName("Многопоточное решение быстрее, чем однопоточное")
-    void test1() {
-        // Given
-        long numberOfIterations = 1_000_000_000;
-
-        // When
-        var first = System.nanoTime();
-        Task4.oneThreadPI(numberOfIterations);
-        var expected = System.nanoTime() - first;
-        first = System.nanoTime();
-        Task4.multiThreadPI(numberOfIterations);
-        var actual = System.nanoTime() - first;
-
-        // Then
-        assertTrue(actual < expected);
-    }
-
     @Test
     @DisplayName("Отклонение от числа pi меньше нормы")
-    void test2() {
+    void test1() {
         // Given
         long numberOfIterations = 100_000_000;
 
