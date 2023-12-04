eedfbe3fd21d2f976acfd2ded06ab42f98138531
aleckbb <belozerov200@gmail.com>
2023-12-04 05:17:50 +0300

    project4 full

--- src/main/java/edu/project4/Runner.java
+++ src/main/java/edu/project4/Runner.java
@@ -11,12 +11,12 @@ public class Runner {
 
     @SuppressWarnings({"UncommentedMain", "MagicNumber"})
     public static void main(String[] args) {
-        var start = System.nanoTime();
+        //var start = System.nanoTime();
         CreateFlame flame = new CreateFlame(2, 10_000, 3_000, 2.2, true);
         flame.render();
         Pixel[][] image = flame.gammaCorrection(flame.getMatrixDisplay());
-        var end = System.nanoTime() - start;
-        System.out.println(end);
+        //var end = System.nanoTime() - start;
+        //System.out.println(end);
         DrawerFlame drawerFlame = new DrawerFlame(1920, 1080);
         drawerFlame.drawFlame(image);
         drawerFlame.saveToFile();
